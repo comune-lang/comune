b13392cf802993b719c7af43ec5980fb7290a3c0
Ash Taylor <partypoison.channel@gmail.com>
2022-11-05 14:38:21 +0100

    Move module.rs files back to module/mod.rs

---
+++ src/semantic/traits.rs
@@ -0,0 +1,52 @@
+use std::hash::{Hash, Hasher};
+use std::{
+	collections::HashMap,
+	ptr,
+	sync::{Arc, RwLock, Weak},
+};
+
+use super::{
+	namespace::{Identifier, Name, NamespaceEntry},
+	types::Type,
+};
+
+#[derive(Debug, Clone)]
+pub struct TraitRef {
+	pub def: Weak<RwLock<TraitDef>>,
+	pub name: Identifier,
+	pub args: Vec<(Name, Type)>,
+}
+
+#[derive(Debug)]
+pub struct TraitDef {
+	pub items: HashMap<Name, NamespaceEntry>,
+	pub supers: Vec<Identifier>,
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct TraitImpl {
+	pub items: HashMap<Name, NamespaceEntry>,
+}
+
+unsafe impl Sync for TraitRef {}
+
+impl PartialEq for TraitRef {
+	fn eq(&self, other: &Self) -> bool {
+		Arc::ptr_eq(&self.def.upgrade().unwrap(), &other.def.upgrade().unwrap())
+			&& self.name == other.name
+			&& self.args == other.args
+	}
+}
+
+impl Eq for TraitRef {}
+
+impl Hash for TraitRef {
+	fn hash<H: Hasher>(&self, state: &mut H) {
+		ptr::hash(self.def.upgrade().unwrap().as_ref(), state);
+
+		for (name, arg) in &self.args {
+			name.hash(state);
+			arg.hash(state);
+		}
+	}
+}
